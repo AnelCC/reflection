746310cf97af4e92b9b755518b16d0ecda96a2d1
Anel CC <anel.calvo.cervantes@gmail.com>
2018-09-06 15:41:28 -0400

    add BalancedBrackets test class

---
+++ app/src/test/java/com/anelcc/algorithms/HackerRankBalancedBracketsTest.java
@@ -0,0 +1,35 @@
+package com.anelcc.algorithms;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class HackerRankBalancedBracketsTest {
+    private HackerRankBalancedBrackets balancedBrackets;
+
+
+    @Before
+    public void setUp() {
+        balancedBrackets = new HackerRankBalancedBrackets();
+    }
+
+    @Test
+    public void ReturnStringBalancedBracketsHashMap() {
+        assertEquals("yes", balancedBrackets.getBalancedBracketsHashMap("{[()]}"));
+        assertEquals("no", balancedBrackets.getBalancedBracketsHashMap("{[(])}"));
+        assertEquals("yes", balancedBrackets.getBalancedBracketsHashMap("{{[[(())]]}}"));
+      //  assertEquals("yes", balancedBrackets.init("}[(]}[][)({]([][)}[)[]))({(]}{}]{-truncated-}"));
+    }
+
+
+    @Test
+    public void ReturnBooleanBalancedBracketsHashMap() {
+        assertTrue(balancedBrackets.isBalancedBracketsHashMap("(){}[]"));
+        assertFalse(balancedBrackets.isBalancedBracketsHashMap("(({}[]"));
+    }
+}
