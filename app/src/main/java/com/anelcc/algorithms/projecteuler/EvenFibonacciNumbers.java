0496c1c03cc561cac786996fb2f325f5a3baad5d
Anel CC <anel.calvo.cervantes@gmail.com>
2019-08-12 18:48:21 -0500

    [Anel][LeetCode] The Fibonacci sequence

---
+++ app/src/main/java/com/anelcc/algorithms/projecteuler/EvenFibonacciNumbers.java
@@ -0,0 +1,42 @@
+package com.anelcc.algorithms.projecteuler;
+
+/*
+Each new term in the Fibonacci sequence is generated
+by adding the previous two terms. By starting with 1 and 2,
+the first 10 terms will be:
+
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+By considering the terms in the Fibonacci sequence whose values
+do not exceed four million, find the sum of the even-valued terms.
+
+Note: The Fibonacci Sequence is the series of numbers:
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
+1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393
+The Rule is xn = xn-1 + xn-2
+[0, 1, 1, 2, x ] ---> (0 + 0 = [0], 0 + 1 = [1], 1 + 1 = [2])
+ Fn = Fn-1 + Fn-2
+ */
+public class EvenFibonacciNumbers {
+
+// [0, 1, 1, 2, x ] ---> (0 + 0 = [0], 0 + 1 = [1], 1 + 1 = [2])
+    public int fibonacciNumbers() {
+        int num1 = 0;
+        int num2 = 1;
+        int temp = 0;
+        int sum = 0;
+
+        do {
+            if (num2 % 2 == 0) {
+                sum += num2;
+            }
+            temp = num1 + num2;
+            num1 = num2;
+            num2 = temp;
+        } while (num2 < 4000000);
+
+        System.out.println(sum);
+
+        return sum;
+    }
+}
---
+++ app/src/test/java/com/anelcc/algorithms/projecteuler/EvenFibonacciNumbersTest.java
@@ -0,0 +1,26 @@
+package com.anelcc.algorithms.projecteuler;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EvenFibonacciNumbersTest {
+    EvenFibonacciNumbers fibonacciNumbers;
+
+    @Before
+    public void setUp() {
+        fibonacciNumbers = new EvenFibonacciNumbers();
+    }
+
+    @After
+    public void tearDown() {
+        fibonacciNumbers = null;
+    }
+
+    @Test
+    public void fibonacciNumbers() {
+        assertEquals(4613732, fibonacciNumbers.fibonacciNumbers());
+    }
+}
